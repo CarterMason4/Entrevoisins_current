c0645eba7ce82eb54bee4c1bc328879f872fbd46
CarterMason4 <nicotvas@hotmail.fr>
2020-11-17 14:01:17 +0100

    Création du package "ViewModel" et de la classe "FavouriteViewModel"

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/dao/viewModel/FavouriteViewModel.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.entrevoisins.dao.viewModel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+public class FavouriteViewModel extends AndroidViewModel {
+
+
+    public FavouriteViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
