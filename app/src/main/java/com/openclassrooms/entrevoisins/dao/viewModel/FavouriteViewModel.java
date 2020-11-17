08bd21b87b3937fbf3345545584d77523c76cbf9
CarterMason4 <nicotvas@hotmail.fr>
2020-11-17 14:04:30 +0100

    Modification de la classe "FavouriteViewModel" avec ajout des méthodes de d'ajout et de suppression des favoris.

--- app/src/main/java/com/openclassrooms/entrevoisins/dao/viewModel/FavouriteViewModel.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/dao/viewModel/FavouriteViewModel.java
@@ -2,12 +2,35 @@ package com.openclassrooms.entrevoisins.dao.viewModel;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
 import android.support.annotation.NonNull;
 
+import com.openclassrooms.entrevoisins.dao.repository.FavouriteRepository;
+import com.openclassrooms.entrevoisins.model.Favourite;
+
+import java.util.List;
+
 public class FavouriteViewModel extends AndroidViewModel {
+
+    private FavouriteRepository repository;
+    private LiveData<List<Favourite>> allFavourites;
 
 
     public FavouriteViewModel(@NonNull Application application) {
         super(application);
+        repository = new FavouriteRepository(application);
+        allFavourites = repository.getAllFavourites();
+    }
+
+    public void insert(Favourite f) {
+        repository.insert(f);
+    }
+
+    public void delete(Favourite f) {
+        repository.delete(f);
+    }
+
+    public LiveData<List<Favourite>> getAllFavourites() {
+        return allFavourites;
     }
 }
