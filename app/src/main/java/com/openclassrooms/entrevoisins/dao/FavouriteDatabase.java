e2376dec8e2f9c10ff75d164c07a22263256aed2
CarterMason4 <nicotvas@hotmail.fr>
2020-11-17 13:53:07 +0100

    Modification de la classe FavouriteRepository - Ajout de tâches asynchrones pour l'ajout et la suppression de favoris.

--- app/src/main/java/com/openclassrooms/entrevoisins/dao/FavouriteDatabase.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/dao/FavouriteDatabase.java
@@ -1,9 +1,11 @@
 package com.openclassrooms.entrevoisins.dao;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.support.annotation.NonNull;
 
 import com.openclassrooms.entrevoisins.model.Favourite;
 
@@ -26,5 +28,6 @@ public abstract class FavouriteDatabase extends RoomDatabase {
 
         return instance;
     }
-    
+
+
 }
